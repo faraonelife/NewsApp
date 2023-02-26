08d0375cd8578cf816624d8faa8a5bd9faeecb0e
Farxod Khaitbaev <72006448+faraonelife@users.noreply.github.com>
2023-02-26 10:45:02 +0500

    Commit 5: 1)some changes by using MVVM 2)repo changes 3)VM Provider Factory 4)wrapper class to differentiate on success or error responses and to handel loading states

--- app/src/main/java/com/example/newsapp/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/NewsActivity.kt
@@ -2,17 +2,29 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.databinding.ActivityNewsBinding
+import com.example.newsapp.db.ArticleDatabase
+
+import com.example.newsapp.repository.NewsRepository
+import com.example.newsapp.ui.NewsViewModel
+import com.example.newsapp.ui.NewsViewModelProviderFactory
 
 class NewsActivity : AppCompatActivity() {
     private lateinit var binding :ActivityNewsBinding
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val newsRepository=NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory=NewsViewModelProviderFactory(newsRepository)
+        viewModel=ViewModelProvider(this,viewModelProviderFactory).get(NewsViewModel::class.java)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
         binding.bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
     }
---
+++ app/src/main/java/com/example/newsapp/repository/NewsRepository.kt
@@ -0,0 +1,7 @@
+package com.example.newsapp.repository
+
+import com.example.newsapp.db.ArticleDatabase
+
+class NewsRepository ( val db:ArticleDatabase ){
+
+}
